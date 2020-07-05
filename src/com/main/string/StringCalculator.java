8002f7113cd97d942347ed76de29444e517d9617
Aamir Shahab <aamir.shahab007@gmail.com>
2020-07-05 09:30:45 +0530

    Update StringCalculator.java

--- src/com/main/string/StringCalculator.java
+++ src/com/main/string/StringCalculator.java
@@ -61,4 +61,9 @@ public class StringCalculator {
 	public static void main(String[] args) {
 		System.out.println(add("//[**][%%]\n1**2%%3"));
 	}
+	public int div(){
+		int a=20;
+		int b=10;
+		return(a/b);
+	}
 }
